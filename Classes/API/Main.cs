dd6ab45e2b089977a8359171030ba4347e3a6738
matvei <matveikylosov@gmail.com>
2024-12-10 17:32:40 +0500

    Создана сущность Main

---
+++ Classes/API/Main.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kylosov.Classes.API
+{
+    public class Main
+    {
+        public double temp { get; set; }
+        public int humidity { get; set; }
+        public int pressure { get; set; }
+    }
+}
