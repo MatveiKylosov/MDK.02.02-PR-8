c7959da0b6389d838f84d9a3ca298f7d7e922457
matvei <matveikylosov@gmail.com>
2024-12-10 17:34:05 +0500

    Создана сущность Weather

--- Classes/API/WeatherData.cs
+++ Classes/API/WeatherData.cs
@@ -6,7 +6,13 @@ using System.Threading.Tasks;
 
 namespace Kylosov.Classes.API
 {
-    internal class WeatherData
+    public class WeatherData
     {
+        public double Temp { get; set; }
+        public string Description { get; set; }
+        public double WindSpeed { get; set; }
+        public int WindDeg { get; set; }
+        public int Humidity { get; set; }
+        public int Pressure { get; set; }
     }
 }
